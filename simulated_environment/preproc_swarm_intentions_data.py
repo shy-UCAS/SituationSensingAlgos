11918893f987cfe3fd15121133dae3c4a1d6c40b
alfred <puppysnake@126.com>
2024-12-16 16:28:31 +0800

    manual intention trajectories visualization code added

--- simulated_environment/preproc_swarm_intentions_data.py
+++ simulated_environment/preproc_swarm_intentions_data.py
@@ -19,11 +19,16 @@ class SwarmIntentExhibitor(object):
         self.file_path = file_path
         self.file_data = None
 
-        self.uav_ids = None
-        self.uav_xys = None
+        self.uav_ids = None # 无人机的编号
+        self.uav_xys = None # 无人机的轨迹坐标
 
+        self.radar_ids = None # 雷达的编号
         self.radar_locs = None # 雷达的位置
+
+        self.airport_ids = None # 机场的编号
         self.airport_locs = None # 机场的位置
+
+        self.hq_ids = None # 指挥所的编号
         self.hq_locs = None # 指挥所的位置
 
         self.coord_scale = coord_scale
@@ -54,19 +59,61 @@ class SwarmIntentExhibitor(object):
                 _interp_func = interp1d(_orig_trj_ts, _uav_xys[_uav_id, :, _dim], kind="cubic")
                 self.uav_xys[_uav_id, :, _dim] = gaussian_filter1d(_interp_func(_interp_trj_ts), sigma=1)
         
-        # 获取无人机机场的位置
-        
+        # 获取雷达的编号和位置
+        self.radar_ids = np.unique([_col_nm.split('_')[0] for _col_nm in self.file_data.columns
+                                  if re.match(r"radar\d*_(x|y|z)", _col_nm)])
 
-        if vis:
-            fig, ax = plt.subplots(1, 1, figsize=(10, 10))
-            for _uav_i in range(self.uav_xys.shape[0]):
-                ax.plot(self.uav_xys[_uav_i, :, 0], self.uav_xys[_uav_i, :, 1])
-            plt.show()
+        _radar_xs = np.array([self.file_data[_radar_id + "_x"] for _radar_id in self.radar_ids])
+        _radar_ys = np.array([self.file_data[_radar_id + "_y"] for _radar_id in self.radar_ids])
+        self.radar_locs = np.concatenate((_radar_xs[:, :1], _radar_ys[:, :1]), axis=1) * self.coord_scale
+
+        # 获取机场的编号和位置
+        self.airport_ids = np.unique([_col_nm.split('_')[0] for _col_nm in self.file_data.columns
+                                  if re.match(r"UavAirport\d*_(x|y|z)", _col_nm)])
+        
+        _airport_xs = np.array([self.file_data[_airport_id + "_x"] for _airport_id in self.airport_ids])
+        _airport_ys = np.array([self.file_data[_airport_id + "_y"] for _airport_id in self.airport_ids])
+        self.airport_locs = np.concatenate((_airport_xs[:, :1], _airport_ys[:, :1]), axis=1) * self.coord_scale
+
+        # 获取无人机指挥所的位置
+        self.hq_ids = np.unique([_col_nm.split('_')[0] for _col_nm in self.file_data.columns
+                                  if re.match(r"HQ\d*_(x|y|z)", _col_nm)])
+
+        _hq_xs = np.array([self.file_data[_hq_id + "_x"] for _hq_id in self.hq_ids])
+        _hq_ys = np.array([self.file_data[_hq_id + "_y"] for _hq_id in self.hq_ids])
+        self.hq_locs = np.concatenate((_hq_xs[:, :1], _hq_ys[:, :1]), axis=1) * self.coord_scale
 
         # import pdb; pdb.set_trace()
+        # 可视化
+        if vis:
+            fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+
+            # 绘制无人机轨迹
+            for _uav_i in range(len(self.uav_ids)):
+                ax.plot(self.uav_xys[_uav_i, :, 0], self.uav_xys[_uav_i, :, 1])
+            
+            # 绘制雷达位置
+            ax.scatter(self.radar_locs[:, 0], self.radar_locs[:, 1], c="r", marker="x")
+            for _radar_id in range(len(self.radar_ids)):
+                ax.text(self.radar_locs[_radar_id, 0] + 10, self.radar_locs[_radar_id, 1] + 10, self.radar_ids[_radar_id])
+            
+            # 绘制机场位置
+            ax.scatter(self.airport_locs[:, 0], self.airport_locs[:, 1], c="g", marker="o")
+            for _airport_id in range(len(self.airport_ids)):
+                ax.text(self.airport_locs[_airport_id, 0] + 10, self.airport_locs[_airport_id, 1] + 10, self.airport_ids[_airport_id])
+
+            # 绘制无人机指挥所位置
+            ax.scatter(self.hq_locs[:, 0], self.hq_locs[:, 1], c="b", marker="^")
+            for _hq_id in range(len(self.hq_ids)):
+                ax.text(self.hq_locs[_hq_id, 0] + 10, self.hq_locs[_hq_id, 1] + 10, self.hq_ids[_hq_id])
+
+            ax.grid(True, linestyle='--', linewidth=0.5)
+            plt.show()
+
+        
 
 if __name__ == "__main__":
     swarm_intent_dir = r"data\manual_intention_recog"
-    swarm_intent_file = osp.join(swarm_intent_dir, "fast_pass_through_no01.xlsx")
+    swarm_intent_file = osp.join(swarm_intent_dir, "fast_pass_through_no02.xlsx")
 
     intent_exh = SwarmIntentExhibitor(swarm_intent_file, vis=True)
